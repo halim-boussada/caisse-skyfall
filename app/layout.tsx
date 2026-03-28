4ca72144d9003e93f510304089ed93ad1cf1502e
halim-boussada <halimboussada10@gmail.com>
2026-03-28 17:18:49 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/skyfall-logo.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/skyfall-logo-dark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/skyfall-logo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/skyfall-logo.png',
   },
 }
 
